54af51a60b1e9e82fa2e10c71f3fdd00e90c79ec
vmarialuz <vmarialuzm@gmail.com>
2024-01-29 11:40:50 -0500

    Recibir props en un Componente

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../hojas-de-estilo/Testimonio.css'
 
-function Testimonio() {
+function Testimonio(props) {
   return (
     <div className='contenedor-testimonio'>
       <img 
@@ -9,9 +9,9 @@ function Testimonio() {
         src={require('../imagenes/testimonio-emma.png')}
         alt='Foto de Emma'/>
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>Emma Bostian en Suecia</p>
-        <p className='cargo-testimonio'>Software Engineer at Spotify</p>
-        <p className='texto-testimonio'>"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."</p>
+        <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} at {props.empresa}</p>
+        <p className='texto-testimonio'>{props.testimonio}</p>
       </div>
     </div>
   );
